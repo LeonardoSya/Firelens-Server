c095c1bdd743480ad447cfcfbb6396e876eda8f8
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-08-21 15:40:22 +0800

    feat: :zap: deployed mongodb and node.js server

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose');
+const GeoData = require('./models/geoData')
+
+const app = express()
+const port = 3001
+
+// 中间件
+app.use(cors())
+app.use(express.json())
+
+// 连接mongodb
+mongoose.connect('mongodb://localhost:27017/firelens')
+// 检查数据库连接
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB!')
+})
+
+// 路由
+app.get('/api/mapdata', async (req, res) => {
+  try {
+    const { date, day, night } = req.query
+    let filter = {}
+
+    filter.date = date
+
+    if (day && night) {
+    } else if (day) {
+      filter['features.properties.DayNight'] = 100
+    } else if (night) {
+      filter['features.properties.DayNight'] = 0
+    }
+
+    const geoData = await GeoData.find(filter)
+    res.json(geoData)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching data', error })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
---
+++ models/geoData.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// 创建Mongoose模型来匹配GeoJSON数据结构
+const geoDataSchema = new Schema({
+  type: {
+    type: String,
+    emun: ['FeatureCollection'],
+    required: true
+  },
+  features: [
+    {
+      type: {
+        type: String,
+        enum: ['Feature'],
+        required: true
+      },
+      geometry: {
+        geodestic: Boolean,
+        type: {
+          type: String,
+          enum: ['Point', 'LineString', 'Polygon'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+      },
+      id: String,
+      porperties: {
+        Bright_ti5: Number,
+        DayNight: Number,
+        confidence: Number,
+        fire_point: Number,
+        frp: Number,
+      },
+      date: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+
+const GeoData = mongoose.model('GeoData', geoDataSchema, 'feature-collection')
+
+module.exports = GeoData
